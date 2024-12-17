32b6b2fbd59b18434cf9d7e984a939e62004dcf8
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-17 18:30:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-
+import { Provider } from 'react-redux'
+import store from './store'
+import UserInfo from './userInfo'
 import React from "react";
-import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Provider store={store}>
+        <UserInfo />
+      </Provider>
     </div>
   )
 }
